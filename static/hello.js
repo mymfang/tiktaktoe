2f3289dbfcb0313a4d209f39dc5964aab2b98ee4
fang <mymfang2010@gmail.com>
2020-03-21 15:26:25 +0800

    nothing special

--- static/hello.js
+++ static/hello.js
@@ -1,11 +1,27 @@
 document.addEventListener("DOMContentLoaded", function()
 {
     document.querySelector("#countBtn").onclick = count;
+
+    /*
+    document.querySelector("#red").onclick = function()
+    {
+        document.querySelector("#hello").style.color = "red";
+    }
+    */
+
+    document.querySelectorAll(".color-change").forEach(function(button)
+    {
+        button.onclick = function()
+        {
+            document.querySelector("#hello").style.color = button.dataset.color;
+        }
+    });
+
 });
 
 
-let x = 20;
-alert(x);
+//let x = 20;
+// alert(x);
 
 function hello()
 {
